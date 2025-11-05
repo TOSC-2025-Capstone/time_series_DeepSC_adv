fc6ed0acf0975a0102ff94137cb72aa8393675f6
ChanMinLee2 <dlcksals87@gmail.com>
2025-11-05 09:07:37 +0900

    🎨 FEAT : case 10000~10004(  batchsize 1,2,4,8,16,32 테스트 -현재 iT만 학습+테스트 진행)

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -36,8 +36,8 @@ is_preprocessed = True
 is_skip_outlier_eliminate = False
 
 # 학습이 완료되었는지 여부 -> True면 학습된 모델로 평가, False면 학습 진행
-is_trained = True
-# is_trained = False
+# is_trained = True
+is_trained = False
 
 # 성능평가를 진행하지 않게 만듦
 # is_skip_performance = True
@@ -114,7 +114,7 @@ CHANNEL_TYPES = [
 SCALER_TYPES = [scaler.value for scaler in ScalerType]  # ['minmax', 'zscore']
 
 # 현재 사용할 변수 설정들
-model_type = ModelType.DEEPSC.value  # default
+model_type = ModelType.LSTM.value  # default
 loss_type = LossType.MSE.value  # MSE로 설정
 channel_type = ChannelType.RAYLEIGH.value  # no_channel 선택
 scaler_type = ScalerType.ZSCORE.value  # minmax 선택
